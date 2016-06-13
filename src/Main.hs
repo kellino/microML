74bac439438c51c0b2d268027faca9493a864edf
David Kelly <thedavidkelly@hotmail.com>
2016-06-13 15:32:57 +0100

    read from file example

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
 import Repl 
+import Parser
 
 main :: IO ()
-main = repl
+-- main = repl
+main = do
+    res <- parseFromFile exprParser "../tests/simple"
+    case res of
+      Left err -> print err
+      Right xs -> print xs
