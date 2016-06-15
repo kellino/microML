1ffc60cb12f56df9bc167ed1c59d7d389f5c714e
David Kelly <thedavidkelly@hotmail.com>
2016-06-15 14:49:34 +0100

    read from file or repl

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,19 @@
 module Main where
 
 import Repl 
+import Parser
 
-import System.Console.GetOpt
-import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
 
-data Flag 
-    = Verbose | Version | Input String deriving Show
-
-flags :: [OptDescr Flag]
-flags = 
-    [ Option ['f'] ["file"] (OptArg inp "FILE") "input file" ]
-
-inp :: Maybe String -> Flag
-inp = Input . fromMaybe "stdin"
-
-{-parse argv = case getOpt Permute flags argv of-}
-               {-(args , f, []) -> do-}
-                   {-if null f-}
-                      {-then -}
-                      {-else return ()-}
+-- interim version for testing. Possibly to be rewritten with System.Console.GetOpt
 
 main :: IO ()
-main = repl
-{-main = do-}
-    {-res <- parseFromFile exprParser "../tests/simple"-}
-    {-case res of-}
-      {-Left err -> print err-}
-      {-Right xs -> print xs-}
+main = do
+    args <- getArgs
+    if null args
+       then repl
+       else do 
+           res <- parseFromFile exprParser (head args) 
+           case res of
+             Left err -> print err
+             Right xs -> print xs
