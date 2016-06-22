13ec84c606a3c654909c33ca2a4f9ed5fd7d04d8
David Kelly <thedavidkelly@hotmail.com>
2016-06-22 10:59:19 +0100

    state now working

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,9 @@
 module Main where
 
---import Repl 
-import Parser
+import Repl 
 
-import System.Environment (getArgs)
-import Text.Megaparsec
 
 -- interim version for testing. Possibly to be rewritten with System.Console.GetOpt
 
 main :: IO ()
-main = undefined
-
-{-main :: IO ()-}
-{-main = do-}
-    {-args <- getArgs-}
-    {-if null args-}
-       {-then repl-}
-       {-else do -}
-           {-res <- parseFromFile parseProgram (head args)-}
-           {-case res of-}
-             {-Left err -> print $ parseErrorPretty err-}
-             {-Right xs -> print xs-}
+main = repl (return ())
--- src/Repl.hs
+++ src/Repl.hs
@@ -5,9 +5,9 @@ import Parser
 import Eval
 
 import Control.Monad.State.Strict
-import qualified Data.Map as Map
 import System.Console.Repline
-import Data.List (foldl')
+import Data.List (foldl', isPrefixOf)
+import System.IO
 
 data ReplState = ReplState
     { termEnv :: TermEnv }
@@ -26,3 +26,42 @@ liftError (Left err) = do
 evalDecl :: TermEnv -> (String, Expr) -> TermEnv
 evalDecl env (var, body) = termEnv'
     where (val, termEnv') = runEval env var body
+
+exec :: String -> Repl ()
+exec code = do
+    st <- get
+    new <- liftError $ parseProgram "<stdin>" code
+    let st' = st { termEnv = foldl' evalDecl (termEnv st) new }
+    put st'
+    case lookup "it" new of
+      Nothing -> return ()
+      Just x -> do
+          let (val, _) = runEval (termEnv st') "it" x
+          liftIO $ print val
+
+
+cmd = exec
+
+completer :: Monad m => String -> m [String]
+completer n = do
+    let comps = ["true", "false", "and", "or", "not", "otherwise"]
+    return $ filter (isPrefixOf n) comps
+
+options :: [(String, [String] -> Repl ())]
+options = []
+
+initialize :: Repl ()
+initialize = liftIO getArt
+
+getArt :: IO ()
+getArt = do
+    file <- openFile "/home/david/Programming/Haskell/microML/utils/microArt" ReadMode
+    contents <- hGetContents file
+    putStr $ "\ESC[1mWelcome to..." ++ "\n\n" ++ contents ++ "\ESC[0m"
+    hClose file
+
+prompt :: String
+prompt = "\ESC[33mmicroML ⊦\ESC[0m "
+
+repl :: HaskelineT (StateT ReplState IO) a -> IO ()
+repl pre = flip evalStateT initState $ evalRepl prompt cmd options (Word completer) pre
