8c5d15232cd420b4598009159ce67245297c3850
David Kelly <thedavidkelly@hotmail.com>
2016-06-28 19:34:31 +0100

    added some error messages

--- src/Language/Typing/TypeError.hs
+++ src/Language/Typing/TypeError.hs
@@ -1,8 +1,15 @@
 module Language.Typing.TypeError where
 
+import Text.PrettyPrint
+import Language.Typing.Type
+
 data TypeError 
-    = TError String
+    = InfiniteType Type Type
   | UnboundVariable String
+  | Unsolvable Type Type
     deriving (Show)
 
-
+ppError :: TypeError -> Doc
+ppError (UnboundVariable s) = text s
+ppError (InfiniteType t t') = text "Occurs check failed for infinite type" <+> text (show t) <+> text (show t')
+ppError (Unsolvable t t')   = text "Cannot unify types" <+> text (show t) <+> text "and" <+> text (show t')
