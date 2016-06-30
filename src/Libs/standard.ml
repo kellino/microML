92613ff2dc8d107f7bedbb59d1ec32ebfda9aa0e
David Kelly <thedavidkelly@hotmail.com>
2016-06-30 11:26:35 +0100

    added gcd

--- src/Libs/standard.ml
+++ src/Libs/standard.ml
@@ -3,18 +3,22 @@ let id x = x;
 let const x y = x;
 let flip f = \x y -> f y x;
 
-(* boolean functions *)
-let odd? x = if (x % 2 == 1) then true else false;
-let even? x = if (x % 2 == 0) then true else false;
-
 (* print functions *)
 let show x = x;     (* this is just a synonym for id *)
 
 (* maths function *)
-let abs x = if 
-    (x < 0) then (x * -1)
-    else x;
+let abs x = if (x < 0) then (x * -1) else x;
+let max a b = if (a < b) then b else a;
+let min a b = if (a < b) then a else b;
+let square a = a * a;
+let negate x = if (x < 0) then (abs x) else (x * -1);
 
+-- tests --
+let zero? x = if (x == 0) then true else false;
+let odd? x = if (x % 2 == 1) then true else false;
+let even? x = if (x % 2 == 0) then true else false;
+
+-- we need to have lets and where to hide definitions
 let gcd' a b = 
     if (b == 0) then a
     else (gcd' b (a % b));
