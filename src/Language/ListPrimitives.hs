185648b606d9c9d8e7a9bd19269a093b36fbbff2
David Kelly <thedavidkelly@hotmail.com>
2016-07-01 14:22:58 +0100

    added error messages

--- src/Language/ListPrimitives.hs
+++ src/Language/ListPrimitives.hs
@@ -2,23 +2,26 @@ module Language.ListPrimitives  where
 
 import Language.Syntax
 import Control.Monad.Except
--- import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
 
--- type ErrorMonad = Either MLError 
+type ThrowsError = Either MLError
 
--- car :: Expr -> ErrorMonad Expr
-car (List (x:_)) = x
--- car (List []) = throwError $ Default $ T.pack "empty list"
+car :: Expr -> ThrowsError Expr
+car (List (x:_)) = return x
+car (List []) = throwError $ Default $ L.pack "empty list"
 
-cdr :: Expr -> Expr
-cdr (List (_:xs)) = List xs
+cdr :: Expr -> ThrowsError Expr
+cdr (List []) = throwError $ Default $ L.pack "cannot take the tail of an empty list"
+cdr (List [_]) = return $ List []
+cdr (List (_:xs)) = return $ List xs
+cdr _ = throwError $ Default $ L.pack "bad arg"
 
---cons :: Expr -> Expr -> Either MLError Expr
+cons :: Expr -> Expr -> Expr
 cons x1 (List []) = List [x1]
 cons x1 (List [x2]) = List [x1, x2]
 cons x1 (List xs) = List $ x1 : xs
---cons _ _ = throwError $ Default $ T.pack "illegal cons operation: are you sure the second element is a list?"
+-- cons _ _ = throwError $ Default $ L.pack "illegal cons operation: are you sure the second element is a list?"
 
-init' :: Expr -> Expr
-init' (List []) = error "nothing"
-init' (List xs) = List $ init xs
+init' :: Expr -> ThrowsError Expr
+init' (List []) = throwError $ Default $ L.pack "the beginning of an empty list doesn't make any sense"
+init' (List xs) = return $ List $ init xs
