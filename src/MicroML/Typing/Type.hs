33f42a8019abbf19b3d9d57e4d563b407a979591
David Kelly <thedavidkelly@hotmail.com>
2016-07-06 18:23:28 +0100

    wip

--- src/MicroML/Typing/Type.hs
+++ src/MicroML/Typing/Type.hs
@@ -12,10 +12,8 @@ data Type
 data TypeScheme = Forall [TVar] Type
     deriving (Show, Eq, Ord)
 
-typeNum :: Type
 typeNum       = TCon "\ESC[31mNumber\ESC[0m"
 typeBool      = TCon "\ESC[32mBoolean\ESC[0m"
 typeString    = TCon "\ESC[33mString\ESC[0m"
 typeChar      = TCon "\ESC[34mChar\ESC[0m"
 typeList      = TCon "\ESC[35mList\ESC[0m"
-typeEmptyList = TCon "\ESC[35mEmpty List\ESC[35m"
