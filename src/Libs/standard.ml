720836ab03a530d030e62eea541cc505f6de79a7
David Kelly <thedavidkelly@hotmail.com>
2016-07-07 20:19:52 +0100

    rewrite filter with foldr

--- src/Libs/standard.ml
+++ src/Libs/standard.ml
@@ -15,10 +15,17 @@ let cons a b = a : b;
 let length xs = if (end? xs) then 0 else (1 + (length (tail xs)));
 let drop n xs = if (zero? n) then xs else (drop (n-1) (tail xs));
 let take n xs = if (zero? n) then ([]) else ((head xs) : (take (n-1) (tail xs)));
+
+(* folds *)
 let foldr f acc xs = if (end? xs) then acc else ((f (head xs)) (foldr f acc (tail xs)));
 let foldl f acc xs = if (end? xs) then acc else (foldl f (f acc (head xs)) (tail xs));
+(* folds with accumulator as first part of list *)
+let foldr1 f xs = foldr f (head xs) xs;
+let foldrl1 f xs = foldl f (head xs) xs;
+
 let map f xs = foldr (\x xs' -> (f x) : xs') ([]) xs;
 let filter p xs = foldr (\x y -> if (p x) then (x:y) else y) ([]) xs;
+--let length xs = foldr (\x -> x + 1) 0 xs;
 let until p f x = if (p x) then x else (until p f (f x));
 
 (* print functions *)
