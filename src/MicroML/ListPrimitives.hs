fc415e442c028a81d10e3ce9b9307d81dbc04f85
David Kelly <thedavidkelly@hotmail.com>
2016-07-08 09:51:27 +0100

    added Strings

--- src/MicroML/ListPrimitives.hs
+++ src/MicroML/ListPrimitives.hs
@@ -12,27 +12,30 @@ enumFromTo_ _ _                                 = error "doesn't make any sense"
 
 car :: Expr -> Expr
 car (List (x:_))  = x
+car (Lit (LString xs)) = Lit. LChar $ head xs
 car (List [])     = error "head of empty list"
 
 cdr :: Expr -> Expr
-cdr (List [])     = error "empty list"
-cdr (List [_])    = List []
-cdr (List (_:xs)) = List xs
+cdr (List [])          = error "empty list"
+cdr (List [_])         = List []
+cdr (List (_:xs))      = List xs
+cdr (Lit (LString xs)) = Lit . LString $ tail xs
 
 append' :: Expr -> Expr -> Expr
-append' (List xs) (List ys) = List $ xs ++ ys
+append' (List xs) (List ys)                   = List $ xs ++ ys
 append' (Lit (LString xs)) (Lit (LString ys)) = Lit $ LString $ xs ++ ys
 
 init' :: Expr -> Expr
-init' (List [])         = error "emtpy list"
-init' (List [_])        = List []
-init' (List xs)         = List $ init xs
+init' (List [])          = error "emtpy list"
+init' (List [_])         = List []
+init' (List xs)          = List $ init xs
 init' (Lit (LString xs)) = Lit $ LString $ init xs
 
 cons :: Expr -> Expr -> Expr
 cons x1 (List []) = List [x1]
 cons x1 (List [x2]) = List [x1, x2]
 cons x1 (List xs) = List $ x1 : xs
+cons _ _ = error ""
 
 compose :: Expr -> Expr -> Expr
 compose a b = Lit $ LString $ show a ++ " " ++ show b
