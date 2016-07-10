30e2b6efae343fb213641fbd98c230d013c45f0d
David Kelly <thedavidkelly@hotmail.com>
2016-07-10 20:29:55 +0100

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,33 @@
 module Main  where
 
+--import MicroML.Parser
 import Repl.Repl
--- import System.Environment (getArgs)
+--import Jit.Codegen
+--import Jit.Emit
 
+--import Control.Monad (void)
+import System.Environment (getArgs)
+--import qualified Data.Text.Lazy as L
+
+--import qualified LLVM.General.AST as AST
+
+{-initModule :: AST.Module-}
+{-initModule = emptyModule "microMLJit"-}
+
+{-process :: AST.Module -> String -> IO (Maybe AST.Module)-}
+{-process modo source = do-}
+    {-let res = parseProgram source-}
+    {-case res of-}
+      {-Left err -> void $ print err-}
+      {-Right ex -> do-}
+          {-ast <- codegen-}
+          {-return $ Just ast-}
+
+--processFile fn = L.readFile fn >>= process initModule
 
 main :: IO ()
-main = shell
-
-{-main :: IO ()-}
-{-main = do-}
-  {-args <- getArgs-}
-  {-case args of-}
-    {-[]      -> shell (return ())-}
-    {-[fname] -> shell (using [fname])-}
-    {--- ["test", fname] -> shell (using [fname] >> browse [] >> quit ())-}
-    {-_ -> putStrLn "invalid arguments"-}
+main = do
+    args <- getArgs
+    case args of
+      []    -> shell
+      --[fname] -> void $ processFile fname 
