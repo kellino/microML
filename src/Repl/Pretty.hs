f06a88999a8f427a73007006442638d0cb9508b3
David Kelly <thedavidkelly@hotmail.com>
2016-07-11 07:22:34 +0100

    suppressed orphan warning

--- src/Repl/Pretty.hs
+++ src/Repl/Pretty.hs
@@ -1,6 +1,7 @@
 {-# Language FlexibleInstances #-}
 {-# Language TypeSynonymInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Repl.Pretty  where
 
@@ -39,25 +40,13 @@ instance Pretty TypeScheme where
   ppr p (Forall [] t) = ppr p t
   ppr p (Forall ts t) = text "for all" <+> hcat (punctuate space (map (ppr p) ts)) <> text "." <+> ppr p t
 
-instance Pretty Expr where
-  ppr p (Var a) = ppr p a
-  ppr p (App a b) = parensIf (p > 0) $ ppr (p+1) a <+> ppr p b
-  ppr p (Lam a b) = text "\\" <> ppr p a <+> text  "->" <+> ppr p b
-  ppr p (Let a b c) = text "let" <> ppr p a <+> text  "=" <+> ppr p b <+> text "in" <+> ppr p c
-  ppr p (Lit a) = ppr p a
-  ppr p (FixPoint a) = parensIf (p>0) $ text "fix" <> ppr p a
-  ppr p (If a b c) =
-    text "if" <> ppr p a <+>
-    text "then" <+> ppr p b <+>
-    text "else" <+> ppr p c
-
 instance Pretty Lit where
   ppr _ (LInt i) = integer i
   ppr _ (LDouble d) = double d
   ppr _ (LString str) = text str
   ppr _ (LChar c) = text [c]        -- convert char to a string
-  ppr _ (LBoolean True) = text "True"
-  ppr _ (LBoolean False) = text "False"
+  ppr _ (LBoolean True) = text "true"
+  ppr _ (LBoolean False) = text "false"
 
 instance Show TypeError where
   show (UnificationFail a b) =
@@ -74,21 +63,11 @@ ppscheme = render . ppr 0
 pptype :: Type -> String
 pptype = render . ppr 0
 
-ppexpr :: Expr -> String
-ppexpr = render . ppr 0
-
 ppsig :: (String, TypeScheme) -> String
 ppsig (a, b) = ppLit a ++ " \ESC[35m::\ESC[0m " ++ ppscheme b
---ppsig (a, b) = a ++ " \ESC[35m::\ESC[0m " ++ ppscheme b
-
-{-ppdecl :: (String, Expr) -> String-}
-{-ppdecl (a, b) = "let " ++ a ++ " = " ++ ppexpr b-}
 
 ppenv :: Env -> [String]
 ppenv (TypeEnv env) = map ppsig $ Map.toList env
-
-ppTypeError :: TypeError -> String
-ppTypeError = render . ppError
 
 ppLit :: String -> String
 ppLit a 
@@ -102,7 +81,7 @@ ppLit a
   | "LString" `isInfixOf` a  = bold ++ (init . (!!2) . words) a ++ unbold
   | otherwise                = bold ++ a ++ unbold
 
-
+bold, unbold :: String
 bold = "\ESC[37m"
 unbold = "\ESC[0m"
 
