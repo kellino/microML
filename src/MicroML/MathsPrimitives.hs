8fd57ed88a61f66bb1ab993a675f3778d6936443
David Kelly <thedavidkelly@hotmail.com>
2016-07-12 11:54:23 +0100

    errors working for sqrt

--- src/MicroML/MathsPrimitives.hs
+++ src/MicroML/MathsPrimitives.hs
@@ -2,8 +2,21 @@ module MicroML.MathsPrimitives where
 
 import MicroML.Syntax
 
-sqRoot (Lit (LInt a)) = Lit . LDouble $ sqrt $ realToFrac a
-sqRoot (Lit (LDouble a)) = Lit . LDouble $ sqrt a
+sqrt' :: Expr -> Expr
+sqrt' a = do
+    let res = sqRoot a
+    case res of
+      Just val@(Lit (LDouble x)) -> 
+          if isNaN x 
+             then PrimitiveErr . MathsPrim $ "the square root of a negative number is undefined"
+             else val
+      Nothing -> PrimitiveErr . MathsPrim $ "you didn't enter a number"
 
+sqRoot :: Expr -> Maybe Expr
+sqRoot (Lit (LInt a)) = Just $ Lit . LDouble $ sqrt $ realToFrac a
+sqRoot (Lit (LDouble a)) = Just $ Lit . LDouble $ sqrt a
+sqRoot _ = Nothing 
+
+-- this doesn't work
 log' :: Expr -> Double -> Expr
 log' (Lit (LInt a)) n = Lit . LDouble $ logBase n $ realToFrac a
