8dff3873f38c5645d2012e3ac7fe9f21b099f9aa
David Kelly <thedavidkelly@hotmail.com>
2016-07-13 15:22:11 +0100

    wip

--- src/MicroML/MathsPrimitives.hs
+++ src/MicroML/MathsPrimitives.hs
@@ -1,22 +1,45 @@
 module MicroML.MathsPrimitives where
 
-import MicroML.Syntax
+--import MicroML.Syntax
 
-sqrt' :: Expr -> Expr
-sqrt' a = do
-    let res = sqRoot a
-    case res of
-      Just val@(Lit (LDouble x)) -> 
-          if isNaN x 
-             then PrimitiveErr . MathsPrim $ "the square root of a negative number is undefined"
-             else val
-      Nothing -> PrimitiveErr . MathsPrim $ "you didn't enter a number"
+import Control.Monad.State (State, evalState, get, put)
+import System.Random (StdGen, mkStdGen, randomR)
 
-sqRoot :: Expr -> Maybe Expr
-sqRoot (Lit (LInt a)) = Just $ Lit . LDouble $ sqrt $ realToFrac a
-sqRoot (Lit (LDouble a)) = Just $ Lit . LDouble $ sqrt a
-sqRoot _ = Nothing 
+type R a = State StdGen a
 
--- this doesn't work
-log' :: Expr -> Double -> Expr
-log' (Lit (LInt a)) n = Lit . LDouble $ logBase n $ realToFrac a
+type Seed = Int
+
+runRandom :: R a -> Int -> a
+runRandom action seed = evalState action $ mkStdGen seed
+
+randInt :: Int -> Int -> R Int
+randInt lo hi = do
+    gen <- get
+    let (r, gen') = randomR (lo, hi) gen
+    put gen'
+    return r
+
+randDouble :: R Double
+randDouble = do
+    gen <- get
+    let (r, gen') = randomR (1, 10) gen
+    put gen'
+    return r
+
+{-ints :: Int -> Int -> R [Int]-}
+{-ints lo hi = mapM (randInt lo hi) $ repeat ()-}
+
+{-randInts :: Int -> R [Int]-}
+{-randInts x = fmap (take x) ints-}
+
+doubles :: R [Double]
+doubles = mapM (const randDouble) $ repeat ()
+
+randDoubles :: Int -> R [Double]
+randDoubles x = fmap (take x) doubles
+
+test :: R [Double]
+test = randDoubles 5 
+
+_runD :: Int -> [Double]
+_runD = runRandom test 
