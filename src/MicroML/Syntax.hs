93b3d9f20edcdd284c7a4abdf71cf238bc6eb603
David Kelly <thedavidkelly@hotmail.com>
2016-07-14 10:13:04 +0100

    Show instance for Binop

--- src/MicroML/Syntax.hs
+++ src/MicroML/Syntax.hs
@@ -44,7 +44,32 @@ data Binop =
         OpAdd | OpSub | OpMul | OpDiv | OpIntDiv | OpExp | OpMod 
       | OpOr | OpXor | OpAnd | OpEq | OpLe | OpLt | OpGe 
       | OpGt | OpNotEq | OpCons | OpComp | OpAppend
-      deriving (Eq, Ord, Show)      
+      deriving (Eq, Ord)      
+
+instance Show Binop where
+    show OpAdd    = red ++ "addition" ++ unred
+    show OpSub    = red ++ "subtraction" ++ unred
+    show OpMul    = red ++ "multiplication" ++ unred
+    show OpDiv    = red ++ "division" ++ unred
+    show OpIntDiv = red ++ "integer division" ++ unred
+    show OpExp    = red ++ "exponents" ++ unred
+    show OpMod    = red ++ "modulo" ++ unred
+    show OpOr     = red ++ "inclusive or" ++ unred
+    show OpXor    = red ++ "exclusive or" ++ unred
+    show OpAnd    = red ++ "logical and" ++ unred
+    show OpEq     = red ++ "equals" ++ unred
+    show OpLe     = red ++ "less than or equal to" ++ unred
+    show OpLt     = red ++ "less than" ++ unred
+    show OpGe     = red ++ "greater than or equal to" ++ unred
+    show OpGt     = red ++ "greater than" ++ unred
+    show OpNotEq  = red ++ "not equal to" ++ unred
+    show OpCons   = red ++ "cons" ++ unred
+    show OpComp   = red ++ "composition" ++ unred
+    show OpAppend = red ++ "concatenation" ++ unred
+
+red, unred :: String
+red = "\ESC[31m"
+unred = "\ESC[0m"
 
 data MLError
     = MathsPrim String 
