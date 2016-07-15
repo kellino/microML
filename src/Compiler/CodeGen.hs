fbd1155e7acd13d5ff06f954170e8781612ad178
David Kelly <thedavidkelly@hotmail.com>
2016-07-15 12:16:59 +0100

    simple parse test

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -3,16 +3,28 @@
 module Compiler.CodeGen where
 
 import Language.C.DSL
+import qualified Data.Text.Lazy as L
+import MicroML.Parser
+
+hoistError :: (Monad m, Show a1) => Either a1 a -> m a
+hoistError (Right val) = return val
+hoistError (Left err) = error $ show err
+
+exec :: L.Text -> String
+exec source = do
+    res <- hoistError $ parseProgram "<from_file>" source
+    show res
 
 includes :: String
 includes = "#include <stdio.h>\n\n"
 
+makeMain :: CFunDef
 makeMain = 
     fun [intTy] "main" [] $ hBlock [
         "printf" # [str "hello microML"]                               
     ]
 
-compile :: IO ()
-compile = do
+compile :: L.Text -> IO ()
+compile file = do
     let cFile = "out.c"
-    writeFile cFile $ includes ++ (show . pretty $ makeMain)
+    writeFile cFile $ includes ++ (show . pretty $ makeMain) ++ exec file
