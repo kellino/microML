36c46bd9096b1803b696db149581339e7bfeb6a8
David Kelly <thedavidkelly@hotmail.com>
2016-07-19 14:23:09 +0100

    pp of lists not right

--- src/Repl/Pretty.hs
+++ src/Repl/Pretty.hs
@@ -75,7 +75,8 @@ ppLit :: String -> String
 ppLit a 
   | "Var" `isPrefixOf` a     = bold ++ ((!!1) . words) a ++ unbold
   | "Closure" `isPrefixOf` a = bold ++ "<<closure>>" ++ unbold
-  | "List" `isPrefixOf` a    = "[" ++ bold ++ intercalate ", " (map ppLit (pprList a)) ++ unbold ++ "]"
+  | "List" `isPrefixOf` a    = a
+--  | "List" `isPrefixOf` a    = "[" ++ bold ++ intercalate ", " (map ppLit (pprList a)) ++ unbold ++ "]"
   | "LInt" `isInfixOf` a     = bold ++ noParens ((init . (!!2). words) a) ++ unbold
   | "LDouble" `isInfixOf` a  = bold ++ noParens ((init . (!!2) . words) a) ++ unbold
   | "LBoolean" `isInfixOf` a = bold ++ (init . (!!2) . words) a ++ unbold
