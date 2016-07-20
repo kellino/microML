edc0acdcfc9fa5f6950d7d66027fee6dc1625e20
David Kelly <thedavidkelly@hotmail.com>
2016-07-20 11:43:44 +0100

    fixed simple decs

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -3,31 +3,35 @@
 module Compiler.CodeGen where
 
 import Compiler.MicroBitHeader
-import Compiler.Syntax
+--import Compiler.Syntax
 import MicroML.Syntax
-import MicroML.Parser
+import MicroML.Parser 
 
 import Language.C.DSL 
 import qualified Data.Text.Lazy as L
-import qualified Data.Map as Map
-import Data.Maybe (fromMaybe, mapMaybe)
+{-import qualified Data.Map as Map-}
+import Data.Maybe (mapMaybe)
 import Data.String (fromString)
 
 hoistError :: Show a => Either a t -> t
-hoistError (Right val) = val
+hoistError (Right vl) = vl
 hoistError (Left err) = error $ show err
 
-eval :: (String, Expr) -> CPP
-eval (name, res) = 
-    case res of
-      (Lit (LInt _))     -> Decl $ int "x" .= 1
-      (Lit (LString st)) -> CPPExp $ str st
-      Var x -> CPPExp $ fromMaybe (error "Unbound variable error") $ Map.lookup x microBitAPI
-      App a b -> do
-          let (CPPExp func) = eval (name, a)
-          let (CPPExp arg) = eval (name, b)
-          CPPExp $ func # [arg]
+compileMicroML :: (String, Expr) -> CExtDecl
+compileMicroML (nm, expr) = 
+    case expr of
+      Lit (LInt n) -> export $ int (fromString nm) .= fromInteger n
 
+{-eval :: (String, Expr) -> CPP-}
+{-eval (name, res) = -}
+    {-case res of-}
+      {-(Lit (LInt _))     -> Decl $ int "x" .= 1-}
+      {-(Lit (LString st)) -> CPPExp $ str st-}
+      {-Var x -> CPPExp $ fromMaybe (error "Unbound variable error") $ Map.lookup x microBitAPI-}
+      {-App a b -> do-}
+          {-let (CPPExp func) = eval (name, a)-}
+          {-let (CPPExp arg) = eval (name, b)-}
+          {-CPPExp $ func # [arg]-}
 
 makeMain :: [CExtDecl] -> [(CDecl, Maybe String, CExpr)] -> [CExtDecl]
 makeMain decls inits =
@@ -35,7 +39,7 @@ makeMain decls inits =
     ++ map makeProts inits
     ++ [export $ fun [intTy] "main" [] (makeBlock inits)]
         where makeBlock = hBlock . concatMap buildExp
-              buildExp (_, v, expr) = maybe [expr] ((:[]) . (<-- expr) . fromString) v
+              buildExp (_, v, ex) = maybe [ex] ((:[]) . (<-- ex) . fromString) v
               makeProts = export . (\(a, _, _) -> a)
 
 makeFunProtos :: CExtDecl -> Maybe CExtDecl
@@ -46,14 +50,14 @@ makeFunProtos _ = Nothing
 renderC :: [CExtDecl] -> String
 renderC = concatMap $ show . pretty
 
-compileC :: L.Text -> [CExtDecl] -> IO ()
-compileC nf code = do
+writeCFile :: L.Text -> [CExtDecl] -> IO ()
+writeCFile nf code = do
     let cFile = L.unpack nf ++ ".cpp"
     writeFile cFile $ microBitIncludes ++ renderC code
 
-compiler :: L.Text -> L.Text -> IO ()
-compiler source newFile = do
+compile :: L.Text -> L.Text -> IO ()
+compile source newFile = do
     let res = parseProgram "from source" source
     case res of
-      Right res -> compileC newFile res
-      Left _    -> error "something's gone very wrong"
+      Right prog -> writeCFile newFile $ map compileMicroML prog
+      Left err  -> error $ show err
