d2330ae16fcdb9e2d9ad03a6565ebbddec0fa4d1
David Kelly <thedavidkelly@hotmail.com>
2016-07-21 11:08:02 +0100

    main treated separately

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -3,6 +3,7 @@
 module Compiler.CodeGen where
 
 import Compiler.MicroBitHeader
+import Compiler.CHelper
 import MicroML.Syntax
 import MicroML.Parser 
 
@@ -14,7 +15,7 @@ import Control.Monad.Except
 import qualified Data.Text.Lazy as L
 import Data.Either (rights)
 import qualified Data.Map as Map
-import Data.Maybe (mapMaybe)
+import Data.Maybe (fromJust)
 import Data.String (fromString)
 
 --------------------
@@ -24,14 +25,14 @@ import Data.String (fromString)
 type FuncName = String
 type ErrorMsg = String
 type FileName = L.Text
-type Env = Map.Map String Expr
-type Compiler a = ReaderT Env (ExceptT ErrorMsg Identity) a
+type CompEnv = Map.Map String Expr
+type Compiler a = ReaderT CompEnv (ExceptT ErrorMsg Identity) a
 
 --------------
 -- COMPILER --
 --------------
 
-runCompiler :: Env -> Compiler a -> Either ErrorMsg a
+runCompiler :: CompEnv -> Compiler a -> Either ErrorMsg a
 runCompiler env ev = runIdentity (runExceptT (runReaderT ev env))
 
 hoistError :: Show a => Either a t -> t
@@ -42,44 +43,55 @@ compileMicroML :: (String, Expr) -> Compiler CExtDecl
 compileMicroML (nm, expr) = do
     let newNm = fromString nm
     case expr of
-      -- if there's only a name and a literal, these must be variable declarations
+      -- if there's only a name and a Lit expression, then it must be a variable declaration
       Lit (LInt n)     -> return $ export $ int newNm .= fromInteger n
       Lit (LDouble d)  -> return $ export $ double newNm .= realToFrac d
       Lit (LChar c)    -> return $ export $ char newNm .= chr c
       Lit (LString st) -> return $ export $ charPtr newNm .= str st
+      App (Var n) body -> return $ export $ do
+          let (func, retty) = fromJust $ Map.lookup n microBitAPI
+          if nm == "main" 
+             then makeMain func (resolve body)
+             else fun [getRetType retty] nm [charPtr "str"] $ hBlock [ func # ["str"] ]
+      -- function definition
       Lam n e -> return $ generateLam n e
-      Op op a b -> 
-          case op of
-            OpAdd -> return $ export $ makeAddFunc nm a b
       _ -> throwError "something strange has happened"
 
 generateLam :: String -> Expr -> CExtDecl
-generateLam n exp = undefined
+generateLam _ _ = undefined
 
-makeAddFunc :: String -> Expr -> Expr -> CFunDef
-makeAddFunc name e1 e2 = 
-    case (e1, e2) of
-      (Lit (LInt _), Lit (LInt _)) -> 
-          fun [intTy] name [int "a", int "b"] $ hBlock [
-            creturn ("a" + "b")
-          ]
-
-chr :: Char -> CExpr
-chr = CConst . flip CCharConst undefNode . cChar
-
-makeMain :: [CExtDecl] -> [(CDecl, Maybe String, CExpr)] -> [CExtDecl]
-makeMain decls inits =
-    mapMaybe makeFunProtos decls
-    ++ map makeProts inits
-    ++ [export $ fun [intTy] "main" [] (makeBlock inits)]
-        where makeBlock = hBlock . concatMap buildExp
-              buildExp (_, v, ex) = maybe [ex] ((:[]) . (<-- ex) . fromString) v
-              makeProts = export . (\(a, _, _) -> a)
+getRetType :: RetTy -> CDeclSpec
+getRetType rt = fromJust $ lookup rt table
+    where table = 
+            [ ("void"   , voidTy)
+            , ("int"    , intTy)
+            , ("double" , doubleTy)
+            , ("char"   , charTy) ]
 
 makeFunProtos :: CExtDecl -> Maybe CExtDecl
 makeFunProtos (CFDefExt (CFunDef specs declr _ _ a)) =
     Just . export $ CDecl specs [(Just declr, Nothing, Nothing)] a
 makeFunProtos _ = Nothing
+
+makeMain :: CExpr -> [CExpr] -> CFunDef
+makeMain func bdy = 
+    fun [intTy] "main" [] $ hBlock [
+        microBitInit
+      , func # bdy                            
+      , releaseFiber
+    ]
+
+resolve :: Expr -> [CExpr]
+resolve e = 
+    case e of 
+      Lit (LInt n)     -> [int' n]
+      Lit (LDouble d)  -> [db d]
+      Lit (LChar c)    -> [chr c]
+      Lit (LString st) -> [str st]
+
+---------------------
+-- PRETTY PRINTING --
+---------------------
 
 renderC :: [CExtDecl] -> String
 renderC = newlines . concatMap (show . pretty)
@@ -92,9 +104,15 @@ writeCFile nf code = do
     let cFile = L.unpack nf ++ ".cpp"
     writeFile cFile $ microBitIncludes ++ renderC code
 
+
+----------------------------
+-- MAIN COMPILER FUNCTION --
+----------------------------
+
 compile :: L.Text -> FileName -> IO ()
 compile source fn = do
     let res = parseProgram "from source" source
+    print res
     case res of
       Right prog -> do
           let code = map (runCompiler Map.empty . compileMicroML) prog
