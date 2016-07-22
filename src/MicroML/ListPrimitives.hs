e8df1b009b2a9f465e60497b55e0a5ed2ef30906
David Kelly <thedavidkelly@hotmail.com>
2016-07-22 12:31:58 +0100

    removed errors, now handled by inference

--- src/MicroML/ListPrimitives.hs
+++ src/MicroML/ListPrimitives.hs
@@ -3,11 +3,7 @@ module MicroML.ListPrimitives where
 import MicroML.Syntax
 
 import qualified Data.Char as DC
-import Data.Maybe (fromMaybe)
 
--- TODO add proper exception handling
-
--- used for implementing [? to ?] ranges. Not as flexible as the real implementation in haskell
 enumFromTo_ :: Expr -> Expr -> Expr
 enumFromTo_ (Lit (LInt a)) (Lit (LInt b))         = List $ (Lit . LInt) <$> [a .. b]
 enumFromTo_ (Lit (LDouble a)) (Lit (LDouble b)) = List $ (Lit . LDouble) <$> [a..b]
@@ -15,17 +11,12 @@ enumFromTo_ (Lit (LChar a)) (Lit (LChar b))     = List $ (Lit . LChar) <$> [a..b
 enumFromTo_ _ _                                 = error "doesn't make any sense"         -- temp error message here
 
 car :: Expr -> Expr
-car xs = do
-    let res = car' xs
-    fromMaybe (PrimitiveErr . ListPrim $ "head of an emtpy list") res
-
-car' :: Expr -> Maybe Expr
-car' (List (x:_))  = Just x
-car' (Lit (LString xs)) = Just $ Lit. LChar $ head xs
-car' (List [])     = Nothing
+car (List (x:_)) = x
+car (Lit (LString xs)) = Lit . LChar $ head xs
+car (List []) = PrimitiveErr $ ListPrim "one cannot take the head of an empty list"
 
 cdr :: Expr -> Expr
-cdr (List [])          = error "empty list"
+cdr (List [])          = PrimitiveErr $ ListPrim "one cannot take the tail of an empty list"
 cdr (List [_])         = List []
 cdr (List (_:xs))      = List xs
 cdr (Lit (LString xs)) = Lit . LString $ tail xs
@@ -38,7 +29,8 @@ cons :: Expr -> Expr -> Expr
 cons x1 (List []) = List [x1]
 cons x1 (List [x2]) = List [x1, x2]
 cons x1 (List xs) = List $ x1 : xs
-cons _ _ = error ""
+cons (Lit (LChar a)) (Lit (LString as)) = Lit . LString $ a:as
+cons _ _ = PrimitiveErr $ ListPrim "I suspect the second item is neither a list nor a string..."
 
 compose :: Expr -> Expr -> Expr
 compose a b = Lit $ LString $ show a ++ " " ++ show b
@@ -49,8 +41,8 @@ compose a b = Lit $ LString $ show a ++ " " ++ show b
 
 ord' :: Expr -> Expr
 ord' (Lit (LChar a)) = Lit . LInt . toInteger . DC.ord $ a
-ord' _               = error ""
+ord' _               = PrimitiveErr $ ListPrim "this function only works on type Char"
 
 chr' :: Expr -> Expr
 chr' (Lit (LInt n)) = Lit . LChar $ DC.chr $ fromIntegral n
-chr' _              = error ""
+chr' _               = PrimitiveErr $ ListPrim "this function only works on type Char"
