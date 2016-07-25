6df79e92028fed0416d8bc7f19153a0e58eed562
David Kelly <thedavidkelly@hotmail.com>
2016-07-25 12:26:03 +0100

    initial commit

--- tests/map.ml
+++ tests/map.ml
@@ -1,1 +1,15 @@
-let xs = map (\x -> x + 1) [1 to 10];
+let x = []
+let xs = [1,2,3]
+let t = if xs == [] then 0 else 1
+(* map function *)
+let map f xs =
+    if xs == []
+    then []
+    else (f (head xs)) : (map f (tail xs))
+
+(* foldr *)
+let foldr f z xs =
+    if xs == [] then z
+    else (f x) : foldr f z (tail xs)
+
+let doubled xs = map (\x -> x * 2) xs
