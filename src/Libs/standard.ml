78139a8ad449c71e3ff3a5fd0bfa11ee51a1f3ab
David Kelly <thedavidkelly@hotmail.com>
2016-08-01 16:16:29 +0100

    more added back in

--- src/Libs/standard.ml
+++ src/Libs/standard.ml
@@ -6,15 +6,18 @@ let positive? x = if x > 0 then true else false;
 let negative? x = if x < 0 then true else false;
 (*let end? xs = if xs == [] then true else false;*)
 
+(* print functions *)
+let show x = x;     (* this is just a synonym for id *)
+
 (*(* HOF *)*)
 let id x = x;
 let const x y = x;
-(*let flip f = \x y -> f y x;*)
-(*let succ x = x + 1;*)
-(*let twice f x = f (f x);*)
-(*let cons a b = a : b;*)
-(*let pipe x f = f x;*)
-(*let compose f g x = f (g x);*)
+let succ x = x + 1;
+let flip f = \x y -> f y x;
+let twice f x = f (f x);
+let cons a b = a : b;
+let pipe x f = f x;
+let compose f g x = f (g x);
 (*let length xs = if end? xs then 0 else 1 + (length (tail xs));*)
 (*(*let drop n xs = if (zero? n) then xs else (drop (n-1) (tail xs));*)*)
 (*(*let take n xs = if (zero? n) then ([]) else ((head xs) : (take (n-1) (tail xs)));*)*)
@@ -33,5 +36,3 @@ let const x y = x;
 (*(*let init xs = take ((length xs) - 1) xs;*)*)
 (*let until p f x = if (p x) then x else (until p f (f x));*)
 
-(*(* print functions *)*)
-(*let show x = x;     (* this is just a synonym for id *)*)
