2b64b9a9dd696c9574a5dfb6f786c720618d8d8c
David Kelly <thedavidkelly@hotmail.com>
2016-08-03 15:29:11 +0100

    complete rewrite

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -1,149 +1,110 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Compiler.CodeGen where
 
-import Compiler.MicroBitHeader
 import Compiler.CHelper
-import MicroML.Syntax
-import MicroML.Parser 
-import MicroML.Typing.Env
-import MicroML.Typing.Infer
-import Repl.Pretty()
+import Compiler.MicroBitHeader
 
-import Language.C.DSL 
-import Control.Monad.State.Strict
-import Control.Monad.Identity
-import Control.Monad.Reader
+import MicroML.Parser
+import MicroML.Syntax
+
+import qualified Text.Parsec as TP
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Trans.Either
 import Control.Monad.Except
-import Control.Applicative
+import Control.Monad.Gen 
+import Language.C.DSL
+import Text.Printf
 
 import qualified Data.Text.Lazy as L
-import Data.Either (rights)
 import qualified Data.Map as Map
-import Data.Maybe (fromJust)
 import Data.String (fromString)
 
-import System.Exit
+----------------
+-- DATA TYPES --
+----------------
 
---------------------
--- COMPILER TYPES --
---------------------
+data Var = SVar String | Gen Integer
+    deriving (Eq, Ord, Show)
 
-type FuncName = String
-type CErrorMsg = String
-type FileName = L.Text
-type CompEnv = Map.Map String Expr
-type Compiler a = ReaderT CompEnv (ExceptT CErrorMsg Identity) a
--- typing environment
-data TypeState = TypeState { typeState :: Env }
+type CodeGen = WriterT [(CDecl, Maybe String, CExpr)] (StateT (Map.Map Var String) Compiler) 
 
-type CState a = (StateT TypeState IO) a
+type Compiler = StateT Var (EitherT Failure (Gen Integer))
 
---------------
--- COMPILER --
---------------
+data Stage = Parser | TypeCheck | CodeGen
 
-runCompiler :: CompEnv -> Compiler a -> Either CErrorMsg a
-runCompiler env ev = runIdentity (runExceptT (runReaderT ev env))
+type Loc = String
+type Info = String
+
+data Failure = Failure 
+          { state :: Stage
+          , location :: Loc
+          , summary :: Info }
 
 hoistError :: Show a => Either a t -> t
 hoistError (Right vl) = vl
 hoistError (Left err) = error $ show err
 
-generate :: (String, Expr) -> Compiler CExtDecl
+tellError :: Failure -> String
+tellError Failure{..} =   
+    printf "Error: failure while %s at %s. \n" stateS location summary
+    where stateS = case state of
+                    Parser -> "parsing" 
+                    TypeCheck -> "typechecking" 
+                    CodeGen -> "generating C code"
+
+failGen :: MonadError Failure m => Loc -> Info -> m a
+failGen loc info = throwError $ Failure CodeGen loc info
+
+compileMicroML :: Either TP.ParseError [(String, Expr)] -> Compiler [CExtDecl]
+compileMicroML res = 
+    case res of
+      Left _  -> undefined
+      Right r -> codegen r
+
+codegen :: [(String, Expr)] -> Compiler [CExtDecl]
+codegen = flip evalStateT Map.empty 
+        . fmap (uncurry makeMain) 
+        . runWriterT 
+        . mapM generate
+
+makeMain :: [CExtDecl] -> [(CDecl, Maybe String, CExpr)] -> [CExtDecl]
+makeMain decls _ = 
+    export (fun [intTy] "main" [] $ hBlock [
+            microBitInit
+          , releaseFiber
+    ]) : decls
+
+generate :: (String, Expr) -> CodeGen CExtDecl
 generate (nm, expr) = 
-    case expr of
-      -- if there's only a name and a Lit expression, then it must be a variable declaration
-      Lit _            -> return $ generateLit nm expr
-      --If cond tr fls   -> return $ ternary (gen cond) (gen tr) (gen fls)
-      Nil              -> undefined
-      App (Var n) body -> return $ export $ do
-          let (func, retty) = fromJust $ Map.lookup n microBitAPI
-          if nm == "main" 
-             then makeMain func (resolve body)
-             else fun [getRetType retty] nm [charPtr "str"] $ hBlock [ func # ["str"] ]
-      -- function definition
-      Lam n e -> return $ generateLam n e
-      _ -> throwError "something strange has happened"
-
-gen :: Expr -> CExpr
-gen = undefined
-
-generateLam :: String -> Expr -> CExtDecl
-generateLam _ _ = undefined
-
--- generates simple variable declarations of the form
--- int x = 2;
-generateLit :: String -> Expr -> CExtDecl
-generateLit nm e1 = 
     let newNm = fromString nm
-     in case e1 of  
-          (Lit (LInt n))     -> export $ int newNm .= fromInteger n
-          (Lit (LDouble d))  -> export $ double newNm .= realToFrac d
-          (Lit (LChar c))    -> export $ char newNm .= chr c
-          (Lit (LString st)) -> export $ charPtr newNm .= str st
-
-getRetType :: RetTy -> CDeclSpec
-getRetType rt = fromJust $ Prelude.lookup rt table
-    where table = 
-            [ ("void"   , voidTy)
-            , ("int"    , intTy)
-            , ("double" , doubleTy)
-            , ("char"   , charTy) ]
-
-makeFunProtos :: CExtDecl -> Maybe CExtDecl
-makeFunProtos (CFDefExt (CFunDef specs declr _ _ a)) =
-    Just . export $ CDecl specs [(Just declr, Nothing, Nothing)] a
-makeFunProtos _ = Nothing
-
-makeMain :: CExpr -> [CExpr] -> CFunDef
-makeMain func bdy = 
-    fun [intTy] "main" [] $ hBlock [
-        microBitInit
-      , func # bdy                            
-      , releaseFiber
-    ]
-
-resolve :: Expr -> [CExpr]
-resolve e = 
-    case e of 
-      Lit (LInt n)     -> [int' n]
-      Lit (LDouble d)  -> [db d]
-      Lit (LChar c)    -> [chr c]
-      Lit (LString st) -> [str st]
-
-checkTypes :: [(String, Expr)] -> CState ()
-checkTypes prog = do
-    st <- get
-    let typeState' = hoistError $ inferTop (typeState st) prog
-    let st' = st { typeState = typeState' `mappend` typeState st }
-    put st'
-
----------------------
--- PRETTY PRINTING --
----------------------
+     in case expr of
+          Lit (LInt n)     -> return $ export $ int newNm .= fromInteger n
+          Lit (LDouble d)  -> return $ export $ double newNm .= realToFrac d
+          Lit (LChar c)    -> return $ export $ char newNm .= chr c
+          Lit (LString st) -> return $ export $ charPtr newNm .= str st
+          Nil              -> return undefined
+          x                -> failGen (show x) ""
 
 renderC :: [CExtDecl] -> String
-renderC = newlines . concatMap (show . pretty)
+renderC = unlines . map (show . pretty)
 
-newlines :: String -> String
-newlines xs = [c | v <- xs, c <- if v == ';' || v == '}' then v:"\n" else [v]]
+runCompiler :: Compiler a -> Either Failure a
+runCompiler = runGen
+             . eitherT (return . Left) (return . Right)
+             . flip evalStateT (SVar "")
 
 writeCFile :: L.Text -> [CExtDecl] -> IO ()
-writeCFile nf code = do
-    let cFile = L.unpack nf ++ ".cpp"
+writeCFile dest code = do
+    let cFile = L.unpack dest ++ ".cpp"
     writeFile cFile $ microBitIncludes ++ renderC code
 
-----------------------------
--- MAIN COMPILER FUNCTION --
-----------------------------
-
 compile :: L.Text -> L.Text -> IO ()
-compile source fn = do
-    let res = hoistError $ parseProgram "from source" source
-    mapM_ print res
-    if null res
-       then die $ red ++ "Exit Failure: " ++ unred ++ "the given file was empty, so there's nothing to compile!"
-       else do 
-           let code = map (runCompiler Map.empty . generate) res
-           writeCFile fn $ rights code
+compile source dest = do
+    let res = parseProgram "from source" source
+    let compRes = runCompiler (compileMicroML res) 
+    case compRes of
+      Right ccode -> writeCFile dest ccode
+      Left e -> putStrLn (tellError e)
