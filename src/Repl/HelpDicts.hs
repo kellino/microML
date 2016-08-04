06158ff4dd72ae83d74697ff21b70cfadaea3d5e
David Kelly <thedavidkelly@hotmail.com>
2016-08-04 16:01:27 +0100

    adding defs for standard

--- src/Repl/HelpDicts.hs
+++ src/Repl/HelpDicts.hs
@@ -4,11 +4,14 @@ import qualified Data.Map as Map
 
 standard :: Map.Map String String
 standard = Map.fromList 
-    [ ("compose", "\n         ***compose***       \n\n" ++
+    [ ("compose", "\n       ***     compose     ***\n\n" ++
                   "** compose :: (b -> c) -> (a -> b) -> a -> c**\n" ++ 
                   "compose takes two unary functions and links the results together\n" ++
                   "like this: a to a1 to a2\n" ++
                   "#Example:#\n" ++ "    compose succ succ 3 to 5\n" ++
                   "It works like this\n result = 5 <- (succ 4 = 5) <- (succ 3 = 4) <- 3\n\n") 
-    , ("unary", "\nhaving only one parameter\n")
+      , ("unary", "\nhaving only one parameter, like **id (id :: a -> a)**, so id 5 = 5\n\n")
+      , ("succ",  "\n    ***     succ    ***\n\n" ++ 
+                  "** succ :: Number -> Number**\n" ++
+                  "#Example:#\n" ++ "    succ 3 = 4\n\n")
     ] 
