8fa3033d83fbfe1ff08b52e9ce37f4d69c45830e
David Kelly <thedavidkelly@hotmail.com>
2016-08-05 15:30:53 +0100

    to be removed...

--- src/Repl/HelpDicts.hs
+++ src/Repl/HelpDicts.hs
@@ -6,7 +6,7 @@ standard :: Map.Map String String
 standard = Map.fromList 
     [ ("compose", "\n       ***     compose     ***\n\n" ++
                   "** compose :: (b -> c) -> (a -> b) -> a -> c**\n" ++ 
-                  "compose takes two unary functions and links the results together\n" ++
+                  "compose takes two __unary__ functions and links the results together\n" ++
                   "like this: a to a1 to a2\n" ++
                   "#Example:#\n" ++ "    compose succ succ 3 to 5\n" ++
                   "It works like this\n result = 5 <- (succ 4 = 5) <- (succ 3 = 4) <- 3\n\n") 
@@ -14,4 +14,7 @@ standard = Map.fromList
       , ("succ",  "\n    ***     succ    ***\n\n" ++ 
                   "** succ :: Number -> Number**\n" ++
                   "#Example:#\n" ++ "    succ 3 = 4\n\n")
+      , ("help", "\n        ***     Help    ***         \n\n" ++
+                 "**Welcome to microML help**\n\n"
+        )
     ] 
