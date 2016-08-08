6a9e117f501b35a0f35abb7a0996cd3f917a58d1
David Kelly <thedavidkelly@hotmail.com>
2016-08-08 13:40:47 +0100

    initial commit

---
+++ src/Compiler/Failure.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Compiler.Failure where
+
+import Control.Monad.Except
+
+data Stage = Parser | TypeCheck | CodeGen
+
+type Loc = String
+type Info = String
+
+data Failure = Failure 
+          { state :: Stage
+          , location :: Loc
+          , summary :: Info }
+
+tellError :: Failure -> String
+tellError Failure{..} =   
+    "Error: failure while " ++ stateS ++ " " ++ location ++ " " ++ summary
+    --printf "Error: failure while %s at %s. \n" stateS location summary
+    where stateS = case state of
+                    Parser -> "parsing" 
+                    TypeCheck -> "typechecking" 
+                    CodeGen -> "generating C code"
+
+failParse :: MonadError Failure m => Loc -> Info -> m a
+failParse loc info = throwError $ Failure Parser loc info
+
+failGen :: MonadError Failure m => Loc -> Info -> m a
+failGen loc info = throwError $ Failure CodeGen loc info
