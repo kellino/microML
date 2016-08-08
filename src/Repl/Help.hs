786bf7f728b676ab0df340c57bee42c73d2a574c
David Kelly <thedavidkelly@hotmail.com>
2016-08-08 14:39:30 +0100

    escaped more chars

--- src/Repl/Help.hs
+++ src/Repl/Help.hs
@@ -22,12 +22,12 @@ data Markdown =
       deriving (Eq, Show)
 
 funcChars :: Parser Char
-funcChars = oneOf " '?:+<>-=)!.(,;\"\n"
+funcChars = oneOf " '?:+<>-=)!.(][,;\"\n"
 
 funcName :: Parser String
 funcName = do
     void $ string "=="
-    st <- many1 $ alphaNum <|> oneOf "?'_"
+    st <- many1 $ alphaNum <|> oneOf "?'_ "
     void $ string "=="
     return st
 
