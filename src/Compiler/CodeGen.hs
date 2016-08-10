f13b845f582680f2a82ffbdcc45de598224f09b0
David Kelly <thedavidkelly@hotmail.com>
2016-08-10 14:33:12 +0100

    fresh start

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -1,81 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Compiler.CodeGen where
 
-import Compiler.MicroBitHeader
-import Compiler.CallGraph
-import Compiler.Failure
+import qualified Data.Text.Lazy as L
 
-import MicroML.Parser
+import MicroML.Parser (parseProgram)
 import MicroML.Syntax
 
-import qualified Text.Parsec as TP
-import Control.Monad.State
-import Control.Monad.Writer
-import Control.Monad.Trans.Either
-import Control.Monad.Gen 
-import Language.C.DSL
+import Compiler.MicroBitHeader
+--import Compiler.Failure
+import Compiler.CallGraph
 
-import qualified Data.Text.Lazy as L
+import Text.PrettyPrint
+import Text.Parsec (ParseError)
 import qualified Data.Map as Map
 
-----------------
--- DATA TYPES --
-----------------
+parensWithSemi :: Doc -> Doc
+parensWithSemi d = parens d <> semi <> "\n"
 
-data Var = SVar String | Gen Integer
-    deriving (Eq, Ord, Show)
+bracesNewLine :: Doc -> Doc
+bracesNewLine d = braces ("\n\t" <> d) <> "\n"
 
-type CodeGen = WriterT [(CDecl, Maybe String, CExpr)] (StateT (Map.Map Var String) Compiler) 
+writeToFile :: L.Text -> [Doc] -> IO ()
+writeToFile dest code = do
+    let cFile = L.unpack dest ++ ".cpp"
+    let code' = foldr (<>) "" code
+    writeFile cFile $ render (microBitIncludes <> code')
 
-type Compiler = StateT Var (EitherT Failure (Gen Integer))
+codegen :: [(String, Expr)] -> [Doc]
+codegen = map genTopLevel . validDefs . checkForDuplicates
 
-compileMicroML :: Either TP.ParseError [(String, Expr)] -> Compiler [CExtDecl]
+genTopLevel :: (String, Expr) -> Doc
+genTopLevel ("main", expr) = generateMain expr
+
+generateMain :: Expr -> Doc
+generateMain ex = "int main()" <> bracesNewLine (bitInit <> genBody ex <> fiber)
+
+genBody :: Expr -> Doc
+genBody ex = 
+    case ex of
+         (Lit (LInt n))    -> integer n
+         (Lit (LDouble d)) -> double d
+         (Lit (LChar c))   -> char c
+         (Lit (LString x)) -> doubleQuotes $ text x
+         (Var _)           -> undefined
+         (Let nm e1 e2)    -> generateCExpr nm e1 <> genBody e2
+         (App (Var x) xs)  -> do
+             let found = Map.lookup x microBitAPI
+             case found of
+                  Nothing  -> error "instruction not found"
+                  Just r   -> r <> parensWithSemi (genBody xs)
+
+generateCExpr :: Name -> Expr -> Doc
+generateCExpr nm e1 = 
+    getType e1 <> text nm <> " = " <> genBody e1
+
+getType :: Expr -> Doc
+getType (Lit (LInt _)) = "int"
+getType (Lit (LDouble _)) = "double"
+getType (Lit (LString _)) = "ManagedString"
+getType (Lit (LChar _)) = "char"
+getType (Lit (LBoolean _)) = "bool"
+
+
+compileMicroML :: Either ParseError [(String, Expr)] -> [Doc]
 compileMicroML res = 
     case res of
-      Left err -> failParse (show err) ""
-      Right r -> codegen r
-
-codegen :: [(String, Expr)] -> Compiler [CExtDecl]
-codegen = flip evalStateT Map.empty 
-        . fmap (uncurry makeMain) 
-        . runWriterT 
-        . mapM generateCode
-        . validDefs
-        . checkForDuplicates
-
-makeMain :: [CExtDecl] -> [(CDecl, Maybe String, CExpr)] -> [CExtDecl]
-makeMain decls _ = 
-    decls ++
-    [export (fun [intTy] "main" [] $ hBlock [
-            microBitInit
-    ])] 
-
-generateCode :: (String, Expr) -> CodeGen CExtDecl
-generateCode (nm, expr) = do
-    let funcName = nm
-    let body = makeFuncBody expr
-    return $ export $ fun [voidTy] funcName [] $ hBlock [ body ] 
-    
-makeFuncBody :: Expr -> CExpr
-makeFuncBody (Lam x xs) = makeFuncBody xs
-
-renderC :: [CExtDecl] -> String
-renderC = unlines . map (show . pretty)
-
-runCompiler :: Compiler a -> Either Failure a
-runCompiler = runGen
-             . eitherT (return . Left) (return . Right)
-             . flip evalStateT (SVar "")
-
-writeCFile :: L.Text -> [CExtDecl] -> IO ()
-writeCFile dest code = do
-    let cFile = L.unpack dest ++ ".cpp"
-    writeFile cFile $ microBitIncludes ++ renderC code
+         Left err -> error $ show err
+         Right r -> codegen r
 
 compile :: L.Text -> L.Text -> String -> IO ()
 compile source dest filename = do
     let res = parseProgram filename source
-    print res -- for debugging
-    let compRes = runCompiler (compileMicroML res) 
-    case compRes of
-      Right ccode -> writeCFile dest ccode
-      Left e -> putStrLn (tellError e)
+    print res -- debugging
+    let compRes = compileMicroML res
+    writeToFile dest compRes
--- src/Repl/standard.md
+++
@@ -1,9 +0,0 @@
-compose
-**compose**
-** compose :: (b -> c) -> (a -> b) -> a -> c **
-compose takes two unary functions and links the results together
-like this: a to a1 to a2
-#Example:#
-    compose succ succ 3 to 5
-It works like this
-    result = 5 <- (succ 4 = 5) <- (succ 3 = 4) <- 3\n\n") 
