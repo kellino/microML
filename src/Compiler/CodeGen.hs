f4eff12cf28d424a7fe9cd1132535a170a815d75
David Kelly <thedavidkelly@hotmail.com>
2016-08-13 11:30:47 +0100

    now working with monad

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -1,40 +1,104 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Compiler.CodeGen where
+module CodeGen where
+
+import MicroML.Syntax
+import Compiler.MicroBitHeader
 
 import qualified Data.Text.Lazy as L
-
-import MicroML.Parser (parseProgram)
-import MicroML.Syntax
-
-import Compiler.MicroBitHeader
---import Compiler.Failure
-import Compiler.CallGraph
-
---import Control.Monad.State.Strict
-import Text.PrettyPrint
-import Text.Parsec (ParseError)
 import qualified Data.Map as Map
+import Text.PrettyPrint
+import Data.Char (toLower)
+import Text.Parsec (ParseError)
 
-type CodeEnv = Map.Map Name Expr
+import Control.Monad.Identity
+import Control.Monad.Except
 
-data CompilerState a = CompilerState { codeState :: CodeEnv }
+example :: [(String, Expr)]
+example = [("main", App (Var "scroll") (Lit (LString "Hello")))]
+         --, ("hello", App (App (App (Var "compose") (Var "double")) (Var "double")) (Lit (LInt 2)))]
 
-instance Monoid (CompilerState a) where
-    mempty = CompilerState Map.empty
-    mappend (CompilerState a) (CompilerState b) = CompilerState $ Map.union a b
+type Compiler a = ExceptT String Identity a
 
-initState :: CompilerState CodeEnv
-initState = CompilerState Map.empty
+type UserCode = Map.Map String Expr
 
-parensWithSemi :: Doc -> Doc
-parensWithSemi d = parens d <> semi <> "\n"
+genTopLevel :: UserCode -> (String, Expr) -> Compiler Doc
+genTopLevel env ("main", expr) = generateMain env expr
+genTopLevel env (nm, expr) = generateFunc env nm expr
+
+generateMain :: UserCode -> Expr -> Compiler Doc
+generateMain env ex = do
+    ex' <- genBody env ex
+    return $ "init main()" <> bracesNewLine (bitInit <> ex' <> fiber)
+
+generateFunc :: UserCode -> Name -> Expr -> Compiler Doc
+generateFunc env nm ex =  do
+    ex' <- genBody env ex
+    return $ text nm <> "()" <> bracesNewLine ex'
+
+genBody :: UserCode -> Expr -> Compiler Doc
+genBody env ex = 
+    case ex of
+         (Lit (LInt n))     -> return $ integer n
+         (Lit (LDouble d))  -> return $ double d
+         (Lit (LChar c))    -> return $ char c
+         (Lit (LString x))  -> return $ doubleQuotes $ text x
+         (Lit (LBoolean x)) -> return $ text . map toLower . show $ x
+         (Var _)            -> undefined
+         (Let nm e1 e2)     -> do 
+                  e1' <- generateCExpr env nm e1
+                  e2' <- genBody env e2
+                  return $ e1' <> e2'
+
+         (App (Var x) xs)   -> do
+             let found = Map.lookup x microBitAPI
+             case found of
+                  Nothing  -> case Map.lookup x env of
+                                   Nothing -> error "not found"
+                                   Just r -> do -- genBody env r <> parensWithSemi (genBody env xs)
+                                       r1 <- genBody env r
+                                       r2 <- genBody env xs
+                                       return $ r1 <> parensWithSemi r2
+                  Just r   -> do  
+                      r2 <- genBody env xs
+                      return $ r <> parensWithSemi r2
+         x                 -> throwError $ "cannot genBody the expression " ++ show x
 
 semiWithNewLine :: Doc
 semiWithNewLine = semi <> "\n"
 
+parensWithSemi :: Doc -> Doc
+parensWithSemi d = parens d <> semi <> "\n"
+
 bracesNewLine :: Doc -> Doc
 bracesNewLine d = braces ("\n\t" <> d) <> "\n"
+
+generateCExpr :: UserCode -> Name -> Expr -> Compiler Doc
+generateCExpr env nm e1 = do
+    ty <- getType e1
+    let nm' = text nm
+    e1' <- genBody env e1
+    return $ ty <> nm' <> " = " <> e1' <> semiWithNewLine
+
+getType :: Expr -> Compiler Doc
+getType (Lit (LInt _)) = return $ "int" <> space
+getType (Lit (LDouble _)) = return $ "double" <> space
+getType (Lit (LString _)) = return $ "ManagedString" <> space
+getType (Lit (LChar _)) = return $ "char" <> space
+getType (Lit (LBoolean _)) = return $ "bool" <> space
+getType _ = throwError "cannot get type of this expression"
+
+runCompiler :: ExceptT e Identity a -> Either e a
+runCompiler ev = runIdentity (runExceptT ev)
+
+codegen :: [(String, Expr)] -> Compiler [Doc]
+codegen prog = 
+    let code = Map.fromList prog
+        in mapM (genTopLevel code) prog
+
+hoistError :: Either ParseError [(String, Expr)] -> [(String, Expr)]
+hoistError (Right val) = val
+hoistError (Left err) = error $ show err
 
 writeToFile :: L.Text -> [Doc] -> IO ()
 writeToFile dest code = do
@@ -42,49 +106,5 @@ writeToFile dest code = do
     let code' = foldr (<>) "" code
     writeFile cFile $ render (microBitIncludes <> code')
 
-codegen :: [(String, Expr)] -> [Doc]
-codegen = map genTopLevel 
-        . validDefs 
-        . checkForDuplicates
-
-genTopLevel :: (String, Expr) -> Doc
-genTopLevel ("main", expr) = generateMain expr
-
-generateMain :: Expr -> Doc
-generateMain ex = "int main()" <> bracesNewLine (bitInit <> genBody ex <> fiber)
-
-genBody :: Expr -> Doc
-genBody ex = 
-    case ex of
-         (Lit (LInt n))    -> integer n
-         (Lit (LDouble d)) -> double d
-         (Lit (LChar c))   -> char c
-         (Lit (LString x)) -> doubleQuotes $ text x
-         (Var _)           -> undefined
-         (Let nm e1 e2)    -> generateCExpr nm e1 <> genBody e2
-         (App (Var x) xs)  -> do
-             let found = Map.lookup x microBitAPI
-             case found of
-                  Nothing  -> error "not found"
-                  Just r   -> r <> parensWithSemi (genBody xs)
-
-generateCExpr :: Name -> Expr -> Doc
-generateCExpr nm e1 = 
-    getType e1 <> text nm <> " = " <> genBody e1 <> semiWithNewLine
-
-getType :: Expr -> Doc
-getType (Lit (LInt _)) = "int" <> space
-getType (Lit (LDouble _)) = "double" <> space
-getType (Lit (LString _)) = "ManagedString" <> space
-getType (Lit (LChar _)) = "char" <> space
-getType (Lit (LBoolean _)) = "bool" <> space
-
-hoistError :: Either ParseError [(String, Expr)] -> [(String, Expr)]
-hoistError (Right val) = val
-hoistError (Left err) = error $ show err
-
-compile :: L.Text -> L.Text -> String -> IO ()
-compile source dest filename = do
-    let res = hoistError $ parseProgram filename source
-    let compRes = codegen res
-    writeToFile dest compRes
+main :: IO ()
+main = print $ runCompiler $ codegen example
