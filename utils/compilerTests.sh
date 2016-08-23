df4b8ad5de5839b14992483735fe2a67e504d6dc
David Kelly <thedavidkelly@hotmail.com>
2016-08-23 11:50:13 +0100

    initial commit

---
+++ utils/compilerTests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+ml=$(which microML)
+FILES="../test/compiler/*.mml"
+
+for f in $FILES
+do 
+    filename=$(basename "$f")
+    newfile="${filename%.*}"
+    if [ -x "$ml" ]; then
+        printf "attempting to compile \e[1m%s\e[0m\n" "$filename"
+        microML -c "$f" "../test/compiler/res/$newfile"
+    else
+        echo "\e[31mError:\e[0m microML is not in your system path" 
+        break
+    fi
+done
