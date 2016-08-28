233e1a287780dcd769a67d1ab59700a75c6b057b
David Kelly <thedavidkelly@hotmail.com>
2016-08-28 17:58:34 +0100

    output to file

--- utils/compilerTests.sh
+++ utils/compilerTests.sh
@@ -8,10 +8,10 @@ do
     filename=$(basename "$f")
     newfile="${filename%.*}"
     if [ -x "$ml" ]; then
-        printf "attempting to compile \e[1m%s\e[0m\n" "$filename"
-        microML -c "$f" "../test/compiler/res/$newfile"
+        printf "attempting to compile \e[1m%s\e[0m\n" "$filename" >> output.txt
+        microML -c "$f" "../test/compiler/res/$newfile" >> output.txt
     else
         echo "\e[31mError:\e[0m microML is not in your system path" 
         break
     fi
-done
+done 
