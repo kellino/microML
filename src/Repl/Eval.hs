aaf73ba411af516c8a4b6eb905f85347b11dbdea
David Kelly <thedavidkelly@hotmail.com>
2016-08-31 22:00:04 +0100

    eval enum

--- src/Repl/Eval.hs
+++ src/Repl/Eval.hs
@@ -2,12 +2,10 @@ module Repl.Eval where
 
 import MicroML.Syntax
 import MicroML.ListPrimitives
+import MicroML.MathsPrimitives
 
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
-import Data.Bits (xor)
-import Data.List.Split
-import Data.List (isInfixOf)
 
 emptyTmenv :: TermEnv
 emptyTmenv = Map.empty
@@ -64,6 +62,7 @@ eval env expr = case expr of
         let a' = eval env a
         let b' = eval env b
         case op of
+          OpEnum   -> enumFromTo_ a' b'
           OpAdd    -> a' `add` b'
           OpSub    -> a' `sub`  b'
           OpMul    -> a' `mul`  b'
@@ -83,109 +82,6 @@ eval env expr = case expr of
           OpCons   -> a' `cons` b'
           OpAppend -> a' `append` b'
           OpPipe   -> eval env (App b a)
-
-cons :: Expr -> Expr -> Expr
-cons a Nil = BinOp OpCons a Nil
-cons a (BinOp OpCons x Nil) = BinOp OpCons a (BinOp OpCons x Nil)
-cons a ls@(BinOp OpCons _ _) = BinOp OpCons a ls
-cons (Lit (LChar x)) (Lit (LString y)) = Lit . LString $ x : y
-cons x y = error $ show x ++ " " ++ show y
-
-add :: Expr -> Expr -> Expr
-add (Lit (LInt a)) (Lit (LInt b)) = Lit $ LInt $ a + b
-add (Lit (LDouble a)) (Lit (LDouble b)) = Lit $ LDouble $ a + b
-add (Lit (LInt a)) (Lit (LDouble b)) = Lit $ LDouble $ realToFrac a + b
-add (Lit (LDouble a)) (Lit (LInt b)) = Lit $ LDouble $ a + realToFrac b
-
-or', and', xor' :: Expr -> Expr -> Expr
-or' (Lit (LBoolean a)) (Lit (LBoolean b)) = Lit $ LBoolean $ a || b
-and' (Lit (LBoolean a)) (Lit (LBoolean b)) = Lit $ LBoolean $ a && b
-xor' (Lit (LBoolean a)) (Lit (LBoolean b)) = Lit $ LBoolean $ a `xor` b
-
-sub :: Expr -> Expr -> Expr
-sub (Lit (LInt a)) (Lit (LInt b)) = Lit $ LInt $ a - b
-sub (Lit (LDouble a)) (Lit (LDouble b)) = Lit $ LDouble $ a - b
-sub (Lit (LInt a)) (Lit (LDouble b)) = Lit . LDouble . truncate' $ realToFrac a - b
-sub (Lit (LDouble a)) (Lit (LInt b)) = Lit . LDouble . truncate' $ a - realToFrac b
-
-mul :: Expr -> Expr -> Expr
-mul (Lit (LInt a)) (Lit (LInt b)) = Lit $ LInt $ a * b
-mul (Lit (LDouble a)) (Lit (LDouble b)) = Lit . LDouble . truncate' $ a * b
-mul (Lit (LInt a)) (Lit (LDouble b)) = Lit . LDouble . truncate' $ realToFrac a * b
-mul (Lit (LDouble a)) (Lit (LInt b)) = Lit . LDouble . truncate' $ a * realToFrac b
-
-div' :: Expr -> Expr -> Expr
-div' (Lit (LInt a)) (Lit (LInt b)) = Lit $ LDouble $ realToFrac a / realToFrac b
-div' (Lit (LDouble a)) (Lit (LDouble b)) = Lit $ LDouble $ a / b
-div' (Lit (LInt a)) (Lit (LDouble b)) = Lit $ LDouble $ realToFrac a / b
-div' (Lit (LDouble a)) (Lit (LInt b)) = Lit $ LDouble $ a / realToFrac b
-
-intDiv :: Expr -> Expr -> Expr
-intDiv (Lit (LInt a)) (Lit (LInt b)) = Lit . LInt $ a `div` b
-intDiv (Lit (LDouble a)) (Lit (LDouble b)) = Lit $ LInt $ floor a `div` floor b
-intDiv (Lit (LInt a)) (Lit (LDouble b)) = Lit $ LInt $ a `div` floor b
-intDiv (Lit (LDouble a)) (Lit (LInt b)) = Lit $ LInt $ floor a `div` b
-
-mod' :: Expr -> Expr -> Expr
-mod' (Lit (LInt a)) (Lit (LInt b)) = Lit $ LInt $ a `mod` b
-mod' _ _ = error "only works on integers" -- improve this 
-
-exp' :: Expr -> Expr -> Expr
-exp' (Lit (LInt a)) (Lit (LInt b)) = Lit $ LInt $ a^b
-exp' (Lit (LInt a)) (Lit (LDouble b)) = Lit $ LDouble $ realToFrac a**b
-exp' (Lit (LDouble a)) (Lit (LInt b)) = Lit $ LDouble $ a ^ b
-exp' (Lit (LDouble a)) (Lit (LDouble b)) = Lit $ LDouble $ a**b
-
-log' :: Expr -> Expr 
-log' (Lit (LInt a))    = Lit . LDouble $ log $ realToFrac a
-log' (Lit (LDouble a)) = Lit . LDouble $ log a
-
-opEq :: Expr -> Expr -> Expr
-opEq (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a == b
-opEq (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a == realToFrac b
-opEq a b = Lit . LBoolean $ a == b
-
-opLe :: Expr -> Expr -> Expr
-opLe (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a <= b
-opLe (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a <= realToFrac b
-opLe a b = Lit . LBoolean $ a <= b
-
-opLt :: Expr -> Expr -> Expr
-opLt (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a < b
-opLt (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a < realToFrac b
-opLt a b = Lit . LBoolean $ a < b
-
-opGt :: Expr -> Expr -> Expr
-opGt (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a > b
-opGt (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a > realToFrac b
-opGt a b = Lit . LBoolean $ a > b
-
-opGe :: Expr -> Expr -> Expr
-opGe (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a >= b
-opGe (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a >= realToFrac b
-opGe a b = Lit . LBoolean $ a >= b
-
-opNotEq :: Expr -> Expr -> Expr
-opNotEq (Lit (LInt a)) (Lit (LDouble b)) = Lit . LBoolean $ realToFrac a /= b
-opNotEq (Lit (LDouble a)) (Lit (LInt b)) = Lit . LBoolean $ a /= realToFrac b
-opNotEq a b = Lit . LBoolean $ a /= b
-
--- | an abritrary truncation of floating-point rounding errors. It's unlikely that this will be a
--- problem for students. Horrible horrible code though. 
-
--- TODO find an elegant mathematical solution to this problem, rather than nasty string manipulation
-truncate' :: Double -> Double
-truncate' = read . dropZeros . show
-    where dropZeros x = head (split x) ++ "." ++ getValid (head (tail (split x)))
-          split       = splitOn "."
-          getValid s 
-              | "e" `isInfixOf` s  = s
-              | hasform s = if length s == 1 then s else  show $ read [head s] + 1
-              | take 3 s   == "000" = "0"
-              | otherwise  = head s : getValid (tail s) 
-
-hasform :: String -> Bool
-hasform (_:ys) = all (== '9') ys 
 
 runEval :: TermEnv -> String -> Expr -> (Expr, TermEnv)
 runEval env x exp = 
