1491a3276e8bc528e9c01d1472e292abbe1d600a
David Kelly <thedavidkelly@hotmail.com>
2016-08-31 22:00:13 +0100

    new import

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -2,6 +2,7 @@ module EvalSpec where
 
 import Test.Hspec
 import MicroML.ListPrimitives
+import MicroML.MathsPrimitives
 import MicroML.Syntax
 import Control.Exception (evaluate)
 
@@ -12,27 +13,27 @@ evalS = hspec $
     describe "evalS" $ do
         describe "eval" $ do
             it "should eval an int to an int" $
-                eval emptyTmenv (Lit (LInt 1)) `shouldBe` (Lit (LInt 1))
+                eval emptyTmenv (Lit (LInt 1)) `shouldBe` Lit (LInt 1)
             it "should eval a double to a double" $
-                eval emptyTmenv (Lit (LDouble 1.0)) `shouldBe` (Lit (LDouble 1.0))
+                eval emptyTmenv (Lit (LDouble 1.0)) `shouldBe` Lit (LDouble 1.0)
             it "should eval a char to a char" $
-                eval emptyTmenv (Lit (LChar 'a')) `shouldBe` (Lit (LChar 'a'))
+                eval emptyTmenv (Lit (LChar 'a')) `shouldBe` Lit (LChar 'a')
             it "should eval a string to a string" $
-                eval emptyTmenv (Lit (LString "hello")) `shouldBe` (Lit (LString "hello"))
+                eval emptyTmenv (Lit (LString "hello")) `shouldBe` Lit (LString "hello")
             it "should eval a bool to a bool" $
-                eval emptyTmenv (Lit (LBoolean True)) `shouldBe` (Lit (LBoolean True))
+                eval emptyTmenv (Lit (LBoolean True)) `shouldBe` Lit (LBoolean True)
             it "should eval Nil to Nil" $
                 eval emptyTmenv Nil `shouldBe` Nil
 
         describe "add" $ do
             it "should add two integers" $
-                add (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` (Lit (LInt 2))
+                add (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` Lit (LInt 2)
             it "should add two doubles" $
-                add (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 2.2))
+                add (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 2.2)
             it "should add an int to a double" $
-                add (Lit (LInt 1)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 2.1))
+                add (Lit (LInt 1)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 2.1)
             it "should add a double to an int" $
-                add (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` (Lit (LDouble 2.1))
+                add (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` Lit (LDouble 2.1)
 
             -- test for failure, but the type checker should prevent this ever happening
             it "should fail on other input" $
@@ -40,13 +41,13 @@ evalS = hspec $
 
         describe "sub" $ do
             it "should sub two integers" $
-                sub (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` (Lit (LInt 0))
+                sub (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` Lit (LInt 0)
             it "should sub two doubles" $
-                sub (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 0.0))
+                sub (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 0.0)
             it "should sub an int and a double" $
-                sub (Lit (LInt 2)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 0.9))
+                sub (Lit (LInt 2)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 0.9)
             it "should sub a double and an int" $
-                sub (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` (Lit (LDouble 0.1))
+                sub (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` Lit (LDouble 0.1)
 
             -- test for failure, but the type checker should prevent this ever happening
             it "should fail on other input" $
@@ -54,13 +55,13 @@ evalS = hspec $
     
         describe "mul" $ do
             it "should mul two integers" $
-                mul (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` (Lit (LInt 1))
+                mul (Lit (LInt 1)) (Lit (LInt 1)) `shouldBe` Lit (LInt 1)
             it "should mul two doubles" $
-                mul (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 1.21))
+                mul (Lit (LDouble 1.1)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 1.21)
             it "should mul an int and a double" $
-                mul (Lit (LInt 1)) (Lit (LDouble 1.1)) `shouldBe` (Lit (LDouble 1.1))
+                mul (Lit (LInt 1)) (Lit (LDouble 1.1)) `shouldBe` Lit (LDouble 1.1)
             it "should mul a double and an int" $
-                mul (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` (Lit (LDouble 1.1))
+                mul (Lit (LDouble 1.1)) (Lit (LInt 1)) `shouldBe` Lit (LDouble 1.1)
 
         describe "truncate'" $ do
             it "should remove trailing zeros" $
