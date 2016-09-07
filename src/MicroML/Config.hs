d8fa68b3d030373c66100a77f63217c683eeec3a
David Kelly <thedavidkelly@hotmail.com>
2016-09-07 22:10:45 +0100

    wip config

--- src/MicroML/Config.hs
+++ src/MicroML/Config.hs
@@ -1,35 +1,15 @@
-module MicroML.Config where
+module MicroML.Config (loadConfig) where
 
 import Data.ConfigFile
-import qualified Data.Map as Map
-import Control.Monad.Except
---import System.FilePath
---import System.Directory
+import Data.Either.Utils
 
-
-dark :: Map.Map String String
-dark = Map.fromList 
-    [ ("bold", "\ESC[1m")
-    , ("clear", "\ESC[0m")
-    ]
-
-data ConfigInfo = ConfigInfo { colourScheme :: String }
-                deriving Show
-
-readConfig :: String -> IO ConfigInfo
-readConfig f = do
-    rv <- runExceptT $ do
-        cp <- join $ liftIO $ readfile emptyCP f
-        let x = cp
-
-        cs <- get x "ColourScheme" "terminal"
-
-        return ConfigInfo { colourScheme = cs }
-
-    either (error . snd) return rv
+loadConfig :: FilePath -> IO ()
+loadConfig file = do
+    conf <- readfile emptyCP file
+    let cp = forceEither conf
+    putStrLn "Your setting is: "
+    let config = forceEither $ items cp "colourscheme"
+    print config
 
 {-main :: IO ()-}
-{-main = do-}
-    {-home <- getHomeDirectory-}
-    {-conf <- readConfig $ home </> ".microMLrc"-}
-    {-print conf-}
+{-main = config-}
